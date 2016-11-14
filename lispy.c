d11b915789bab75ffeddb8b1c7af82723f10aa69
Jonathan Simpson <jsimpson.github@gmail.com>
2016-11-13 18:14:27 -0800

    start parsing s-expressions

--- lispy.c
+++ lispy.c
@@ -7,13 +7,18 @@ typedef struct
 {
     int type;
     long num;
-    int err;
+    char *err;
+    char *sym;
+    int count;
+    struct lval **cell;
 } lval;
 
 enum
 {
     LVAL_NUM,
-    LVAL_ERR
+    LVAL_ERR,
+    LVAL_SYM,
+    LVAL_SEXPR
 };
 
 enum
@@ -23,20 +28,63 @@ enum
     LERR_BAD_NUM
 };
 
-lval lval_num(long x)
+lval *lval_num(long x)
 {
-    lval v;
-    v.type = LVAL_NUM;
-    v.num = x;
+    lval *v = malloc(sizeof(lval));
+    v->type = LVAL_NUM;
+    v->num = x;
     return v;
 }
 
-lval lval_err(int x)
+lval *lval_err(char *m)
 {
-    lval v;
-    v.type = LVAL_ERR;
-    v.err = x;
+    lval *v = malloc(sizeof(lval));
+    v->type = LVAL_ERR;
+    v->err = malloc(strlen(m) + 1);
+    strcpy(v->err, m);
     return v;
+}
+
+lval *lval_sym(char *s)
+{
+    lval *v = malloc(sizeof(lval));
+    v->type = LVAL_SYM;
+    v->sym = malloc(strlen(s) +1);
+    strcpy(v->sym, s);
+    return v;
+}
+
+lval *lval_sexpr(void)
+{
+    lval *v = malloc(sizeof(lval));
+    v->type = LVAL_SEXPR;
+    v->count = 0;
+    v->cell = NULL;
+    return v;
+}
+
+void lval_del(lval *v)
+{
+    switch(v->type)
+    {
+        case LVAL_NUM:
+            break;
+        case LVAL_ERR:
+            free(v->err);
+            break;
+        case LVAL_SYM:
+            free(v->sym);
+            break;
+        case LVAL_SEXPR:
+            for (int i = 0; i < v->count; i++)
+            {
+                lval_del(v->cell[i]);
+            }
+            free(v->cell);
+            break;
+    }
+
+    free(v);
 }
 
 void lval_print(lval v)
@@ -124,19 +172,21 @@ lval eval(mpc_ast_t *t)
 int main(int argc, char **argv)
 {
     mpc_parser_t *Number = mpc_new("number");
-    mpc_parser_t *Operator = mpc_new("operator");
+    mpc_parser_t *Symbol = mpc_new("symbol");
+    mpc_parser_t *Sexpr = mpc_new("sexpr");
     mpc_parser_t *Expr = mpc_new("expr");
     mpc_parser_t *Lispy = mpc_new("lispy");
 
     mpca_lang(
         MPCA_LANG_DEFAULT,
-        "                                                 \
-            number : /-?[0-9]+/ ;                         \
-            operator : '+' | '-' | '*' | '/' ;            \
-            expr : <number> | '(' <operator> <expr>+ ')' ; \
-            lispy : /^/ <operator> <expr>+ /$/ ;          \
+        "                                                \
+            number : /-?[0-9]+/ ;                        \
+            symbol : '+' | '-' | '*' | '/' ;             \
+            sexpr  : '(' <expr>* ')' ;                   \
+            expr : <number> | '(' <symbol> <expr>+ ')' ; \
+            lispy : /^/ <expr>+ /$/ ;                    \
         ",
-        Number, Operator, Expr, Lispy
+        Number, Symbol, Sexpr, Expr, Lispy
     );
 
     puts("Lispy version 0.0.0.0.3");
@@ -163,7 +213,7 @@ int main(int argc, char **argv)
         free(input);
     }
 
-    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+    mpc_cleanup(4, Number, Symbol, Sexpr, Expr, Lispy);
 
     return 0;
 }
